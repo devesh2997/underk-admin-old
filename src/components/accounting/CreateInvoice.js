0df5502b015661bd244d4f684146d8305a1b06fb
Kumar Kshitij <34840437+kumar-kshitij@users.noreply.github.com>
2020-05-08 00:42:03 +0530

    updated CreateInvoice

--- src/components/accounting/CreateInvoice.js
+++ src/components/accounting/CreateInvoice.js
@@ -8,18 +8,97 @@ import {
 	Form,
 	FormGroup,
 	Input,
-	Label
+	Label,
+	Row,
+	Col
 } from 'reactstrap';
 import axios from 'axios';
+
+const EVALUATE_PINCODE_URL = (pincode) => `https://us-central1-underk-firebase.cloudfunctions.net/publicApp/pincode?pincode=${pincode}`;
+const CHECK_PINCODE_URL = (pincode) => `https://us-central1-underk-firebase.cloudfunctions.net/checkPincodeAvailability?pincode=${pincode}`;
+
+const INITIAL_STATE = {
+	city: '',
+	state: '',
+	products: [
+		{ key: 1, sku: '', quantity: 1 },
+	],
+	error: null,
+	isLoading: false
+}
 
 export default class CreateInvoice extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			error: null,
-			isLoading: false
+			...INITIAL_STATE
 		};
+
+		this.lastProductKey = 1;
+	}
+
+	onTextChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	}
+
+	onPincodeChange = event => {
+		const pincode = event.target.value;
+		if(pincode.match(/^\d{6}$/)) {
+			axios.get(EVALUATE_PINCODE_URL(pincode))
+			.then(response => {
+				// console.log(response);
+				const { city, state } = response.data;
+				this.setState({ city, state });
+			})
+			.catch(error => {
+				// console.log(error);
+			});
+
+			axios.get(CHECK_PINCODE_URL(pincode))
+			.then(response => {
+				// console.log(response);
+				if(response.data.invalid_pincode) {
+					this.setState({
+						error: new Error('Sorry, we are not currently delivering in your pincode area.')
+					});
+				}
+			})
+			.catch(error => {
+				// console.log(error);
+			});
+		}
+	}
+
+	onProductInfoChange = (event, key) => {
+		let products = Array.from(this.state.products);
+		let targetIdx = products.findIndex(product => product.key === key);
+		products[targetIdx] = {
+			...products[targetIdx],
+			[event.target.name]: event.target.value
+		};
+		this.setState({ products });
+	}
+
+	onProductDelete = key => {
+		let products = Array.from(this.state.products);
+		if(products.length <= 1) return;
+		let targetIdx = products.findIndex(product => product.key === key);
+		products.splice(targetIdx, 1);
+		this.setState({ products });
+	}
+
+	onProductAdd = () => {
+		let products = Array.from(this.state.products);
+		this.lastProductKey++;
+		products.push({
+			key: this.lastProductKey,
+			sku: '',
+			quantity: 1
+		});
+		this.setState({ products });
 	}
 
 	onSubmit = async event => {
@@ -37,24 +116,32 @@ export default class CreateInvoice extends Component {
 				state: event.target.state.value,
 				pincode: event.target.pincode.value,
 			},
-			productSKU: event.target.productSKU.value,
-			productQuantity: Number(event.target.productQuantity.value)
+			products: this.state.products
+				.filter(product => product.sku.length > 0)
+				.map(product => ({
+					sku: product.sku,
+					quantity: Number(product.quantity)
+				})),
+			additionalDiscount: Number(event.target.additionalDiscount.value)
 		}
-
+		console.log(data);
 		try {
+			// const url = 'http://localhost:5001/underk-firebase/us-central1/adminApp/generateInvoiceWithExternalData';
 			const url = 'https://us-central1-underk-firebase.cloudfunctions.net/adminApp/generateInvoiceWithExternalData';
 			const response = await axios.post(url, data);
 			if(!response.data.success) {
 				throw new Error(response.data.message);
 			}
-			window.open(response.data.downloadURL);
+			console.log(response.data);
+			window.open(response.data.invoice.downloadURL);
 		} catch(error) {
-			this.setState({ error, isLoading: false });
+			this.setState({ error });
 		}
+		this.setState({ isLoading: false });
 	}
 
 	render() {
-		const { error, isLoading } = this.state;
+		const { city, state, products, error, isLoading } = this.state;
 
 		return (
 			<Card>
@@ -95,6 +182,15 @@ export default class CreateInvoice extends Component {
 								/>
 							</FormGroup>
 							<FormGroup>
+								<Label>Pincode</Label>
+								<Input type="text"
+									name="pincode"
+									placeholder="Enter pincode"
+									onChange={this.onPincodeChange}
+									required
+								/>
+							</FormGroup>
+							<FormGroup>
 								<Label>Building</Label>
 								<Input type="text"
 									name="building"
@@ -114,7 +210,9 @@ export default class CreateInvoice extends Component {
 								<Label>City</Label>
 								<Input type="text"
 									name="city"
+									value={city}
 									placeholder="Enter city"
+									onChange={this.onTextChange}
 									required
 								/>
 							</FormGroup>
@@ -122,34 +220,80 @@ export default class CreateInvoice extends Component {
 								<Label>State</Label>
 								<Input type="text"
 									name="state"
+									value={state}
 									placeholder="Enter state"
-									required
-								/>
-							</FormGroup>
-							<FormGroup>
-								<Label>Pincode</Label>
-								<Input type="text"
-									name="pincode"
-									placeholder="Enter pincode"
+									onChange={this.onTextChange}
 									required
 								/>
 							</FormGroup>
 						</div>
+						<div>
+							<h5>Products</h5>
+							{products.map(product => (
+								<Row
+									key={product.key}
+									style={{ alignItems: 'flex-end' }}
+								>
+									<Col sm={7}>
+										<FormGroup>
+											<Label>SKU</Label>
+											<Input type="text"
+												name="sku"
+												value={product.sku}
+												onChange={(e) => this.onProductInfoChange(e, product.key)}
+												placeholder="Enter sku"
+												required
+											/>
+										</FormGroup>
+									</Col>
+									<Col sm={3}>
+										<FormGroup>
+											<Label>Quantity</Label>
+											<Input type="number"
+												name="quantity"
+												value={product.quantity}
+												onChange={(e) => this.onProductInfoChange(e, product.key)}
+												placeholder="Enter quantity"
+												min="1"
+												required
+											/>
+										</FormGroup>
+									</Col>
+									{products.length > 1
+										? (
+											<Col sm={2}>
+												<FormGroup>
+													<Button
+														type="button"
+														color="danger"
+														onClick={() => this.onProductDelete(product.key)}
+													>
+														<i className="fa fa-trash" />
+													</Button>
+												</FormGroup>
+											</Col>
+										)
+										: null
+									}
+								</Row>
+							))}
+							<FormGroup className="text-center">
+								<Button
+									type="button"
+									color="secondary"
+									onClick={this.onProductAdd}
+								>
+									<i className="fa fa-plus" />
+								</Button>
+							</FormGroup>
+						</div>
 						<FormGroup>
-							<Label>Product SKU</Label>
-							<Input type="text"
-								name="productSKU"
-								placeholder="Enter productSKU"
-								required
-							/>
-						</FormGroup>
-						<FormGroup>
-							<Label>Product Quantity</Label>
+							<Label>Additional Discount (in paise)</Label>
 							<Input type="number"
-								name="productQuantity"
-								defaultValue="1"
-								placeholder="Enter productQuantity"
-								min="1"
+								name="additionalDiscount"
+								defaultValue="0"
+								placeholder="Enter additional discount"
+								min="0"
 								required
 							/>
 						</FormGroup>
@@ -170,7 +314,13 @@ export default class CreateInvoice extends Component {
 							</Button>
 							<Button type="button"
 								color="secondary"
-								onClick={() => document.forms.createInvoiceForm.reset()}
+								onClick={() => {
+									document.forms.createInvoiceForm.reset();
+									this.setState({
+										...INITIAL_STATE
+									});
+									this.lastProductKey = 1;
+								}}
 							>
 								Reset
 							</Button>
